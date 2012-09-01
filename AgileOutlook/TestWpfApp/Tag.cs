ec486e52167bb376c19b639f763d01b72d6af11f
Ishwor Mali <ishanbuddy@gmail.com>
2012-09-01 17:30:02 +0545

    test listbox for tag

---
+++ AgileOutlook/TestWpfApp/Tag.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace TestWpfApp
+{
+    public class Tag : INotifyPropertyChanged
+    {
+
+        private string tagSource;
+        public string TagSource 
+        {
+            get 
+            {
+                return tagSource;
+            }
+            set
+            {
+                tagSource = value;
+                RaisePropertyChanged("TagSource");
+            }
+        }
+
+        private string tagName;
+        public string TagName
+        {
+            get
+            {
+                return tagName;
+            }
+            set
+            {
+                tagName = value;
+                RaisePropertyChanged("TagName");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ AgileOutlook/TestWpfApp/TagCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace TestWpfApp
+{
+    public class TagCollection : ObservableCollection<Tag>
+    {
+
+    }
+}

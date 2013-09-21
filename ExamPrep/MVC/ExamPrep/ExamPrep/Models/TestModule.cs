debbf9d759dd29f84c671fc0fd9745f0b55eeea9
Ishwor Mali <ishanbuddy@gmail.com>
2013-09-21 11:52:37 +0545

    commit after conflict

---
+++ ExamPrep/MVC/ExamPrep/ExamPrep/Models/TestModule.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web;
+
+namespace ExamPrep.Models
+{
+    public class TestModule : IHttpModule
+    {
+        /// <summary>
+        /// You will need to configure this module in the Web.config file of your
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: http://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region IHttpModule Members
+
+        public void Dispose()
+        {
+            //clean-up code here.
+        }
+
+        public void Init(HttpApplication context)
+        {
+            // Below is an example of how you can handle LogRequest event and provide 
+            // custom logging implementation for it
+            context.LogRequest += new EventHandler(OnLogRequest);
+            context.BeginRequest += context_BeginRequest;
+            context.EndRequest += context_EndRequest;
+        }
+
+        void context_EndRequest(object sender, EventArgs e)
+        {
+            var app = sender as HttpApplication;
+            app.Response.Write("ENding Test Module ");
+        }
+
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            var app = sender as HttpApplication;
+            app.Response.Write("Beginning Test Module ");
+        }
+
+        #endregion
+
+        public void OnLogRequest(Object source, EventArgs e)
+        {
+            //custom logging logic can go here
+        }
+    }
+}

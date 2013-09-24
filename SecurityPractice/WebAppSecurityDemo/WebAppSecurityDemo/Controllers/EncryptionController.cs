231d8d42dc46fa422e7e71b4eec2bf9f362ee9e7
Ishwor Mali <ishanbuddy@gmail.com>
2013-09-25 01:16:50 +0545

    data enc practices

---
+++ SecurityPractice/WebAppSecurityDemo/WebAppSecurityDemo/Controllers/EncryptionController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppSecurityDemo.Models;
+
+namespace WebAppSecurityDemo.Controllers
+{
+    public class EncryptionController : Controller
+    {
+        //
+        // GET: /Encryption/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Encrypt(string input)
+        {
+            var encrypted = Encryption.Encrypt(input);
+            return encrypted;
+        }
+
+        public string Decrypt(string input)
+        {
+            var decrypted = Encryption.Decrypt(input);
+            return decrypted;
+        }
+
+    }
+}
---
+++ SecurityPractice/WebAppSecurityDemo/WebAppSecurityDemo/Models/Encryption.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace WebAppSecurityDemo.Models
+{
+    public class Encryption
+    {
+        public static string Encrypt(string input)
+        {
+            var saltBytes = System.Text.Encoding.Unicode.GetBytes(salt);
+            var inputBytes = System.Text.Encoding.Unicode.GetBytes(input);
+            var db = new Rfc2898DeriveBytes(password, saltBytes);
+            var strm = new MemoryStream();
+            var algorithm = Rijndael.Create();
+            algorithm.Key = db.GetBytes(32);
+            algorithm.IV = db.GetBytes(16);
+            var cs = new CryptoStream(strm, algorithm.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputBytes, 0, inputBytes.Length);
+            cs.FlushFinalBlock();
+            var outputBytes = strm.ToArray();
+            return Convert.ToBase64String(outputBytes);
+
+        }
+
+        public static string Decrypt(string input)
+        {
+            var saltBytes = System.Text.Encoding.Unicode.GetBytes(salt);
+            var inputBytes = Convert.FromBase64String(input);
+            var db = new Rfc2898DeriveBytes(password, saltBytes);
+            var strm = new MemoryStream();
+            var algorithm = Rijndael.Create();
+            algorithm.Key = db.GetBytes(32);
+            algorithm.IV = db.GetBytes(16);
+            var cs = new CryptoStream(strm, algorithm.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputBytes, 0, inputBytes.Length);
+            cs.FlushFinalBlock();
+            var outputBytes = strm.ToArray();
+            return System.Text.Encoding.Unicode.GetString(outputBytes);
+        }
+
+
+        private const string salt = "this is my salt";
+        public const string password = "this is the password then";
+    }
+}

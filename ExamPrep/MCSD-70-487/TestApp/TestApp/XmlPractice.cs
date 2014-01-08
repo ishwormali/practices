c9587964e8fa71436ade7e5f090363163ee4c0e9
Ishwor Mali <ishanbuddy@gmail.com>
2014-01-08 20:06:06 +0545

    mcsd xml practices + new project for interview questions

---
+++ ExamPrep/MCSD-70-487/TestApp/TestApp/XmlPractice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestApp
+{
+    class XmlPractice
+    {
+        public void Test()
+        {
+            var path=Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"XMLFile1.xml");
+
+            using (var reader = XmlReader.Create(path))
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType != XmlNodeType.Whitespace)
+                    {
+                        Console.WriteLine(string.Format("{0} Tag: {1}, ........Value={2}",reader.IsStartElement()?"Opening":"Closing", reader.Name, reader.Value));
+                    }
+                }
+
+            } 
+
+        }
+    }
+}
---
+++ InterviewQuestionsCode/InterviewQuestionsCode/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestionsCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public void SaveData()
+        {
+            try
+            {
+                ///Save the data in the database which may throw error
+                
+            }
+            catch (Exception ex)
+            {
+                
+                throw;
+            }
+            finally {
+
+                EndTask();//task that needs to be executed despite of any error
+            }
+        }
+
+        public void SaveData()
+        {
+            try
+            {
+                ///Save the data in the database which may throw error
+
+                EndTask();//task that needs to be executed despite of any error
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+        }
+
+        public void SaveData()
+        {
+            try
+            {
+                ///Save the data in the database which may throw error
+
+            }
+            catch (Exception ex)
+            {
+
+                EndTask();//task that needs to be executed despite of any error
+                throw;
+            }
+
+            EndTask();//code that needs to be executed despite of any error
+        }
+
+
+        public void EndTask()
+        {
+
+        }
+    }
+
+
+}

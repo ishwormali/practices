19567c0d8e26226e96ca62403919c5e7f80881ba
Ishwor Mali <ishanbuddy@gmail.com>
2014-04-03 18:38:53 +0545

    create new note changes sqllite implementation in progress

---
+++ Android/GoogleKeep/GoogleKeep/src/com/practices/googlekeep/DbHelper.java
@@ -0,0 +1,27 @@
+package com.practices.googlekeep;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+	
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static final String CreateTableNote="CREATE TABLE "+TABLE_NOTES "(" +
+			"" +
+			"" +
+			"";
+
+	private static String TABLE_NOTES="NOTES";
+}

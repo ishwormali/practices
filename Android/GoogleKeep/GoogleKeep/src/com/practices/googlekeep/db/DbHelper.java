9075c39f77ce5779d27c2c9e076a70f4886cfc3a
Ishwor Mali <ishanbuddy@gmail.com>
2014-04-04 18:27:19 +0545

    sqlite db integration in progress

---
+++ Android/GoogleKeep/GoogleKeep/src/com/practices/googlekeep/db/DbHelper.java
@@ -0,0 +1,31 @@
+package com.practices.googlekeep.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+	private static final String DATABASE_NAME="GKeepDb.db";
+	private static final int DATABASE_VERSION=1;
+	
+	
+	public DbHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(DbHelperConstants.CreateTableNote);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		db.execSQL(DbHelperConstants.DropTableNote);
+		db.execSQL(DbHelperConstants.CreateTableNote);
+	}
+	
+}
